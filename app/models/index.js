8f45d2d23437dffd09125f209481d79ec3613277
pantuj0bayron <pantuj_bayron@colgalileoantigua.edu.gt>
2026-06-07 21:51:57 -0600

    seguridad y 535

--- app/models/index.js
+++ app/models/index.js
@@ -21,6 +21,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
 });
 const db = {};
 db.Sequelize = sequelize;
-db.alumnos = require('./alumnos.model.js')(Sequelize, sequelize);
+db.alumnos = require('./alumnos.model.js')(sequelize, Sequelize);
+
 module.exports = db;
 

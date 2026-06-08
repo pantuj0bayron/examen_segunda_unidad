def644a6232fa0f8084ed378403f406d80835164
pantuj0bayron <pantuj_bayron@colgalileoantigua.edu.gt>
2026-06-07 20:59:18 -0600

    seguridad y 41

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,6 @@
 const { Pool } = require("pg")
 
 module.exports = {
-  HOST: process.env.DB_HOST,
-  USER: process.env.DB_USER,
-  PASSWORD: process.env.DB_PASSWORD,
-  DB: process.env.DB_NAME,
-  dialect: "postgres", // Neon usa postgres
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+  url: process.env.DATABASE_URL,
+  dialect: "postgres"
 };
--- app/models/index.js
+++ app/models/index.js
@@ -1,9 +1,8 @@
-const { dialect } = require('../config/db.config');
-const { Pool } = require('../config/db.config');
-const dbConfig = require('../config/db.config');
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
+const dbConfig = require("../config/db.config.js");
+const Sequelize = require("sequelize");
+
+// Sequelize recibe la URL directamente como primer argumento
+const sequelize = new Sequelize(dbConfig.url, {
     dialect: dbConfig.dialect,
     dialectOptions: {
         ssl: {
@@ -11,20 +10,19 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
             rejectUnauthorized: false
         }
     },
-    pool: { 
+    pool: {
         max: dbConfig.pool.max,
         min: dbConfig.pool.min,
         acquire: dbConfig.pool.acquire,
         idle: dbConfig.pool.idle
-        
     }
 });
+
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-db.alumnos = require('./alumnos.models.js')(sequelize, Sequelize);
-console.log("¿El modelo alumnos tiene la función create?:", typeof db.alumnos.create === 'function')
-const AlumnoModel = require('./alumnos.models.js');
-db.alumnos = AlumnoModel(sequelize, Sequelize);
-module.exports = db;
 
+// Inicializa el modelo una sola vez
+db.alumnos = require("./alumnos.models.js")(sequelize, Sequelize);
+
+module.exports = db;

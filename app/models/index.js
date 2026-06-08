87b1864fa0c2c9a4c6b6847105e36fb658361598
pantuj0bayron <pantuj_bayron@colgalileoantigua.edu.gt>
2026-06-07 19:02:02 -0600

    seguridad y 15

--- app/models/index.js
+++ app/models/index.js
@@ -4,6 +4,12 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
+  dialectOptions: {
+    ssl: {
+      require: true, // Esto es lo que Neon está pidiendo
+      rejectUnauthorized: false // Necesario para evitar errores de certificados en Render
+    }
+  },
   pool: {
     max: 5,
     min: 0,

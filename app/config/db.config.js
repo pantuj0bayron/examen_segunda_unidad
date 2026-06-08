cbc2c8a346a8029d049b484ff6ccc62b697db1df
pantuj0bayron <pantuj_bayron@colgalileoantigua.edu.gt>
2026-06-07 21:05:24 -0600

    seguridad y 43

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  HOST: process.env.DB_HOST,
-  USER: process.env.DB_USER,
-  PASSWORD: process.env.DB_PASSWORD,
-  DB: process.env.DB_NAME,
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+  url: process.env.DATABASE_URL,
+  dialect: "postgres"
 };
--- app/models/index.js
+++ app/models/index.js
@@ -1,20 +1,14 @@
 const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
+// Sequelize puede usar la URL directamente
+const sequelize = new Sequelize(dbConfig.url, {
   dialect: dbConfig.dialect,
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false
     }
-  },
-  pool: {
-    max: dbConfig.pool ? dbConfig.pool.max : 5, // Seguridad extra: si falla, usa 5
-    min: dbConfig.pool ? dbConfig.pool.min : 0,
-    acquire: dbConfig.pool ? dbConfig.pool.acquire : 30000,
-    idle: dbConfig.pool ? dbConfig.pool.idle : 10000
   }
 });
 

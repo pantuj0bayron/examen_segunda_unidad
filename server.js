6570fe0516f283b96efb19f7506747b079747ad9
pantuj0bayron <pantuj_bayron@colgalileoantigua.edu.gt>
2026-06-07 21:14:17 -0600

    seguridad y 45

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
-db.sequelize.sync();
+db.Sequelize.sync();
 app.get("/", (req, res) => {
     res.json({ message: "mi primera base de datos en la nube" });
 });
-
 require("./app/routes/alumnos.routes")(app);
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
-    console.log(`el servidor esta corriendo en el puerto${PORT}.`);
+    console.log(`el setvidor esta corriendo en el puerto${PORT}.`);
 });
+module.exports = app;
